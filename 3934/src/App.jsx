c94edea72531dee75dbfc153bfd22c1b16a8bebc
Divyansh <divyanshchandel16@gmail.com>
2025-01-19 03:49:13 +0530

    AI Assistant

--- 3934/src/App.jsx
+++ 3934/src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import MyCombinepages from './components/MyCombinepages';
-
+import AIInteractionPopup from './components/Popup';
 function App() {
-  return <MyCombinepages />;
+  return <>
+    <MyCombinepages />
+    <AIInteractionPopup />
+  </>;
 }
 
 export default App;
